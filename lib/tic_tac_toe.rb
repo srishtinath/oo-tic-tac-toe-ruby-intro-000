0c23aa60ca9cc827d21d111948d021fd51ca166c
Srishti Nath <srishti.nath@gmail.com>
2020-04-01 04:51:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,14 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
