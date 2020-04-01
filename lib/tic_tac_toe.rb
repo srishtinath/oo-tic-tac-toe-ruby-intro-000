b5f8596a8b7b18ca6a0270df4a8c088809dca832
Srishti Nath <srishti.nath@gmail.com>
2020-04-01 05:06:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,4 +113,17 @@ def winner
   end
 end
 
+def play
+  until over?
+    turn
+  end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+
+end
+
 end
